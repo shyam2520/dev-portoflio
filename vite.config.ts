5667c6f6110180ee810e35dae895b2d5a1c58107
Shyam Sundar Mylai <mylai.s@northeastern.edu>
2026-02-23 00:14:01 -0500

    fix: package json fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // GitHub Pages will serve from root since repo is Nightmare99.github.io
+  base: '/', // GitHub Pages will serve from root since repo is shyam2520.github.io
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
